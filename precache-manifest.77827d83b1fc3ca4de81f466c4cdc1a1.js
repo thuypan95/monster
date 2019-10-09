4d0378da3b983b13f6279e86581df4d5563688eb
IT Department of VNNG IT Co <vnng-it@Mobiverses-MacBook-Pro-13-Late-2013.local>
2019-10-09 10:18:11 +0700

    Updates

---
+++ precache-manifest.77827d83b1fc3ca4de81f466c4cdc1a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e456894999e7561f313aab98aacaf1ef",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "bc05c743fdedb4c950b5",
+    "url": "/monster/static/css/main.5fcb5409.chunk.css"
+  },
+  {
+    "revision": "8c84992c9904ec36f7a9",
+    "url": "/monster/static/js/2.813314d8.chunk.js"
+  },
+  {
+    "revision": "bc05c743fdedb4c950b5",
+    "url": "/monster/static/js/main.7bea88ff.chunk.js"
+  },
+  {
+    "revision": "0711cfa045024c2f5635",
+    "url": "/monster/static/js/runtime-main.09cfccce.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9037eada06730ba1023a89f7b8395fe6.js"
+  "/monster/precache-manifest.77827d83b1fc3ca4de81f466c4cdc1a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monster/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
