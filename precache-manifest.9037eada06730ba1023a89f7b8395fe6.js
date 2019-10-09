7d30e9c9c91172b910961bc17de7da013a1b8aaa
IT Department of VNNG IT Co <vnng-it@Mobiverses-MacBook-Pro-13-Late-2013.local>
2019-10-09 10:03:55 +0700

    Updates

---
+++ precache-manifest.9037eada06730ba1023a89f7b8395fe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818f7a10f84e660ff389f27fce9249ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b3ae1f15564471dc521",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f8adf732f098c86ef2d5",
+    "url": "/static/js/2.a3c34952.chunk.js"
+  },
+  {
+    "revision": "1b3ae1f15564471dc521",
+    "url": "/static/js/main.8844e022.chunk.js"
+  },
+  {
+    "revision": "55b1f72ad6cc545f0f0a",
+    "url": "/static/js/runtime-main.06b8b5ae.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
